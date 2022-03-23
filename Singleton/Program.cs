4b080041ed68213b4258f958e18559bb107af6ab
dez <mega6ban@mail.ru>
2022-03-23 20:36:35 +0300

    Singleton pattern added

---
+++ Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Singleton
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var console = ServerConsole.CreateServerConsole();
+
+            console.StartConsole();
+
+            console.PrintToServer("Hello", $"Time: {DateTime.Now}", $"New Year in {(new DateTime(DateTime.Now.Year, 12, 31) - DateTime.Now).Days} days!");
+
+            console.StopConsole();
+        }
+    }
+}
---
+++ Singleton/ServerConsole.cs
@@ -0,0 +1,58 @@
+﻿namespace Singleton
+{
+    [Serializable]
+    public class MaxSingletonObjects : Exception
+    {
+        public MaxSingletonObjects() { }
+        public MaxSingletonObjects(string message) : base(message) { }
+        public MaxSingletonObjects(string message, Exception inner) { }
+    }
+
+    public class ServerConsole
+    {
+        private static int _maxObjects = 1;
+
+        #nullable disable
+        [NonSerialized]
+        private static ServerConsole _console = null;
+
+        public bool ConsoleState { get; private set; } = false;
+
+        protected ServerConsole() { }
+
+        public static ServerConsole CreateServerConsole()
+        {
+            if (_console == null)
+            {
+                _console = new ServerConsole();
+                _maxObjects--;
+            }
+
+            if (_maxObjects == -1)
+                throw new MaxSingletonObjects();
+
+            return _console;
+        }
+
+        public void PrintToServer(params string[] args)
+        {
+            if (ConsoleState)
+            {
+                foreach (string arg in args)
+                {
+                    Console.WriteLine(arg);
+                }
+            }
+        }
+
+        public void StopConsole()
+        {
+            ConsoleState = false;
+        }
+        
+        public void StartConsole()
+        {
+            ConsoleState = true;
+        }
+    }
+}
